520a554b6c3af915d7163f9359ce5e62a75395a5
ivanpbf <ivan27rlb@gmail.com>
2019-02-09 09:51:15 -0400

    iniciando

---
+++ src/serviciorestaurante/Cocinero.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serviciorestaurante;
+import serviciorestaurante.Servicio;
+import java.util.concurrent.Semaphore;
+/**
+ *
+ * @author Ivan
+ */
+public class Cocinero extends Thread{
+    
+}
