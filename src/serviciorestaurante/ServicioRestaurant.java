be19d19ca1738a2c90f8b74d975fd31cf6331644
ivanpbf <ivan27rlb@gmail.com>
2019-02-19 15:23:00 -0400

    archivo de texto agregado

--- src/serviciorestaurante/ServicioRestaurant.java
+++ src/serviciorestaurante/ServicioRestaurant.java
@@ -5,7 +5,16 @@
  */
 package serviciorestaurante;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import serviciorestaurante.ServicioInterfaz; //interfaz
 
 /**
@@ -13,38 +22,50 @@ import serviciorestaurante.ServicioInterfaz; //interfaz
  * Aqui ponemos nuestros nombres
  */
 public class ServicioRestaurant { // ojo que esta es la clase principal donde se inicializara el programa y guardara lo importante
-    
+    //Cantidades del archivo
+    static public int tiempo;
+    static public int maxMesonEntradas;
+    static public int maxMesonPlatos;
+    static public int maxMesonPostres;
+    static public int cantInicialEntrada;
+    static public int cantInicialPF;
+    static public int cantInicialPostres;
+    static public int maxCantEntrada;
+    static public int maxCantPF;
+    static public int maxCantPostre;
+    static public int cantInicialMesoneros;
+    static public int cantMaxMesoneros;
     //Cocineros y Mesoneros
-    static public CEntrada CEntradas [] = new CEntrada[3];
-    static public CPlatoFuerte CPlato [] = new CPlatoFuerte[4];
-    static public CPostre CPostres [] = new CPostre[2];
-    static public Mesonero Mesoneros [] = new Mesonero[6];
+    static public CEntrada CEntradas [];
+    static public CPlatoFuerte CPlato [];
+    static public CPostre CPostres [];
+    static public Mesonero Mesoneros [];
     //Mesones
-    static public int mesonEntradas [] = new int [20];
-    static public int mesonPlato [] = new int [30];
-    static public int mesonPostre [] = new int [10];
+    static public int mesonEntradas [];
+    static public int mesonPlato [];
+    static public int mesonPostre [];
     static public int mesonComida [] = new int [200]; //pendiente que no hay limite, por eso decide este
     //Semaforos de Entradas (comida)
     static public Semaphore SEEntrada = new Semaphore(1);
-    static public Semaphore SPEntrada = new Semaphore(mesonEntradas.length);
+    static public Semaphore SPEntrada;
     static public Semaphore SCEntrada  = new Semaphore(0);
     static public int entraE=0;
     static public int saleE=0;
     //Semaforos de Plato Fuerte
     static public Semaphore SEPlato = new Semaphore(1);
-    static public Semaphore SPPlato = new Semaphore(mesonPlato.length);
+    static public Semaphore SPPlato;
     static public Semaphore SCPlato  = new Semaphore(0);
     static public int entraPF=0;
     static public int salePF=0;
     //Semaforos de Postre
     static public Semaphore SEPostre = new Semaphore(1);
-    static public Semaphore SPPostre = new Semaphore(mesonPostre.length);
+    static public Semaphore SPPostre;
     static public Semaphore SCPostre  = new Semaphore(0);
     static public int entraP=0;
     static public int saleP=0;
     //Semaforos de Comida (lo que sirve el mesonero, todo)
     static public Semaphore SEComida = new Semaphore(1);
-    static public Semaphore SPComida = new Semaphore(mesonComida.length);
+    static public Semaphore SPComida;
     static public Semaphore SCComida  = new Semaphore(0);
     static public int entraC=0;
     static public int saleC=0;
@@ -57,27 +78,111 @@ public class ServicioRestaurant { // ojo que esta es la clase principal donde se
     
     
      public static void main(String[] args) {
+        int[] datos = new int[12];
+        datos = leerArchivoDatos();
          //iniciamos todo
          //ojo que estos son los valores predeterminados, esto cambia si existe archivo de objeto o texto
+        tiempo = datos[0];
+        maxMesonEntradas = datos[1];
+        maxMesonPlatos = datos[2];
+        maxMesonPostres = datos[3];
+        cantInicialEntrada = datos[4];
+        cantInicialPF = datos[5];
+        cantInicialPostres = datos[6];
+        maxCantEntrada = datos[7];
+        maxCantPF = datos[8];
+        maxCantPostre = datos[9];
+        cantInicialMesoneros = datos[10];
+        cantMaxMesoneros = datos[11];
+        //luego lo demas
+        CEntradas = new CEntrada[maxCantEntrada];
+        CPlato = new CPlatoFuerte[maxCantPF];
+        CPostres = new CPostre[maxCantPostre];
+        Mesoneros = new Mesonero[cantMaxMesoneros];
+        mesonEntradas = new int [maxMesonEntradas];
+        mesonPlato = new int [maxMesonPlatos];
+        mesonPostre = new int [maxMesonPostres];
+        SPEntrada = new Semaphore(mesonEntradas.length);
+        SPPlato = new Semaphore(mesonPlato.length);
+        SPPostre = new Semaphore(mesonPostre.length);
+        SPComida = new Semaphore(mesonComida.length);
         servicio.setVisible(true);
         
         //comenzamos las cantidades iniciales
-        CEntradas[0] = new CEntrada(mesonEntradas, SEEntrada, SEEntrada, SCEntrada, entraE, saleE, servicio);
-        CEntradas[0].start();
-        servicio.getCocinerosEntradas().setText("1"); //la cantidad inicial
+        for(int i = 0; i < cantInicialEntrada; i++){
+            CEntradas[i] = new CEntrada(mesonEntradas, SEEntrada, SEEntrada, SCEntrada, entraE, saleE, servicio);
+            CEntradas[i].start();
+        }
+        servicio.getCocinerosEntradas().setText(Integer.toString(cantInicialEntrada)); //la cantidad inicial
 
-        for(int i = 0; i < 2; i++){
-            CPlato[i] = new CPlatoFuerte(mesonPlato, SEPlato, SEPlato, SCPlato, entraE, saleE, servicio);
+        for(int i = 0; i < cantInicialPF; i++){
+            CPlato[i] = new CPlatoFuerte(mesonPlato, SEPlato, SEPlato, SCPlato, entraPF, salePF, servicio);
             CPlato[i].start();
             //set numero en interfaz
         }
-        servicio.getCocinerosPlatos().setText("2");
-        //la cantidad de cocineros de postre inicial es 0
-        servicio.getCocinerosPostres().setText("0");
-        //tampoco nos dan una cantidad inicial de ensambladores, asi que usemos 1?
-        Mesoneros[0] = new Mesonero(SEComida, SPComida, SCComida, entraC, saleC, SEEntrada, SPEntrada, SCEntrada, entraE, saleE,
+        servicio.getCocinerosPlatos().setText(Integer.toString(cantInicialPF));
+        for(int i = 0; i < cantInicialPostres; i++){
+            CPostres[i] = new CPostre(mesonPostre, SEPostre, SEPostre, SCPostre, entraP, saleP, servicio);
+            CPostres[i].start();
+            //set numero en interfaz
+        }
+        servicio.getCocinerosPostres().setText(Integer.toString(cantInicialPostres));
+
+        for(int i = 0; i < cantInicialMesoneros; i++){
+            Mesoneros[i] = new Mesonero(SEComida, SPComida, SCComida, entraC, saleC, SEEntrada, SPEntrada, SCEntrada, entraE, saleE,
                 SEPlato, SPPlato, SCPlato, entraPF, salePF, SEPostre, SPPostre, SCPostre, entraP, saleP, servicio, mesonEntradas, mesonPlato, mesonPostre, mesonComida);
-        Mesoneros[0].start();
-        servicio.getMesoneros().setText("1");
+        Mesoneros[i].start();
+        }
+        
+        servicio.getMesoneros().setText(Integer.toString(cantInicialMesoneros));
+        jefemesoneros.start();
+        gerente.start();
      }
+    public static int[] leerArchivoDatos(){ //lee las columnas filas y minas predeterminadas
+        int[] datos = new int[12];
+        //linea 1 tiempo en segundos que dura una hora
+        //linea 2 capacidad meson entradas
+        //linea 3 capacidad meson de platos fuertes
+        //linea 4 capacidad meson postres
+        //linea 5 cant inicial cocineros de entrada
+        //linea 6 cant inicial cocineros de pf
+        //linea 7 cant inicial cocineros de postres
+        //linea 8 cant max cocineros de entrada
+        //linea 9 cant max cocineros de pf
+        //linea 10 cant max cocineros de postres
+        //linea 11 cant inicial mesoneros
+        //linea 12 cantidad maxima de mesoneros
+        String datosausar = "Datos.txt";
+        String linea = null;
+        try{
+            Scanner scanner = new Scanner(new File("Datos.txt"));
+            int i = 0;
+            while(scanner.hasNextInt()){
+                datos[i] = scanner.nextInt();
+                i++;
+            }
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("No se puede abrir el archivo " +datosausar);                
+        }
+        catch(IOException ex) {
+            System.out.println("Error leyendo el archivo "+datosausar);             
+        }
+        return datos;
+    }
+    /*public static void escribirArchivo(int columnas, int filas, int minas){ //escribe el archivo de texto por si no hay o el jugador quiero usar otras reglas
+        String datos = "Datos.txt";
+        try {
+            FileWriter fileWriter = new FileWriter(datos);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write((new Integer(columnas).toString()));
+            bufferedWriter.newLine();
+            bufferedWriter.write((new Integer(filas).toString()));
+            bufferedWriter.newLine();
+            bufferedWriter.write((new Integer(minas).toString()));
+            bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println("Error escribiendo el archivo "+datos);
+        }*/ //esto es viejo, lo dejo aqui por si acaso hay que poner que la persona lo cambie DENTRO del programa
 }
